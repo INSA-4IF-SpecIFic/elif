022dedd9a4b59e448cc4820f25e462f34a5dac96
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-04 11:16:53 +0100

    Adds the compilor_cmd argument to compilation.Compilation

--- src/compilation.py
+++ src/compilation.py
@@ -5,14 +5,14 @@ import tempfile
 
 class Compilation(object):
 
-    def __init__(self, code):
+    def __init__(self, code, compilor_cmd='g++'):
         source_file = tempfile.mktemp(suffix='.cpp', prefix='elif_code_')
         self.exec_file = tempfile.mktemp(prefix='elif_exec_')
 
         with open(source_file, 'w') as f:
             f.write(code)
 
-        process_arguments = ['g++', '-x', 'c++', '-o', self.exec_file, source_file]
+        process_arguments = [compilor_cmd, '-x', 'c++', '-o', self.exec_file, source_file]
         process = subprocess.Popen(process_arguments, stderr=subprocess.PIPE)
         process.wait()
 
