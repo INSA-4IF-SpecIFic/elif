dec62bd40e358a8c529c88087eaff81cdd988e16
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-02-04 15:02:14 +0100

    Styled exercise description

--- src/static/js/editor.js
+++ src/static/js/editor.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+    /* Editor initialization and configuration */
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/textmate");
     editor.setFontSize(14);
+    editor.setShowPrintMargin(false);
     editor.getSession().setMode("ace/mode/c_cpp");
 
 });
