dcd21e2859c2227466eda85edbe956e7edb90143
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-04 11:07:49 +0100

    Lets compilation.Compilation remove the temporary source code file

--- src/compilation.py
+++ src/compilation.py
@@ -1,17 +1,18 @@
 
+import os
 import subprocess
 import tempfile
 
 class Compilation(object):
 
     def __init__(self, code):
-        self.source_file = tempfile.mktemp(suffix='.cpp', prefix='elif_code_')
-        self.dest_file = tempfile.mktemp(prefix='elif_exec_')
+        source_file = tempfile.mktemp(suffix='.cpp', prefix='elif_code_')
+        self.exec_file = tempfile.mktemp(prefix='elif_exec_')
 
-        with open(self.source_file, 'w') as f:
+        with open(source_file, 'w') as f:
             f.write(code)
 
-        process_arguments = ['g++', '-x', 'c++', '-o', self.dest_file, self.source_file]
+        process_arguments = ['g++', '-x', 'c++', '-o', self.exec_file, source_file]
         process = subprocess.Popen(process_arguments, stderr=subprocess.PIPE)
         process.wait()
 
@@ -21,5 +22,5 @@ class Compilation(object):
         for l in process.stderr:
             self.stderr += l
 
-        print self.stderr
+        os.remove(source_file)
 
