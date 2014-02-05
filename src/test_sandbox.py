0d4b78c7db64e76d0137c61c706d636be8b2b853
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-05 17:15:28 +0100

    Adds Sandbox security test unit

--- src/test_sandbox.py
+++ src/test_sandbox.py
@@ -12,17 +12,24 @@ def test_root_dir():
     del s
     assert not os.path.isdir(root_dir)
 
-def test_ls():
+def test_basic_ls():
     s = Sandbox()
 
     shutil.copy("/bin/ls", s.root_directory + "bin/")
-
     p = s.process([s.root_directory + "bin/ls", "/"], stdout=subprocess.PIPE)
     p.wait()
-
     stdout = p.stdout.read()
 
     assert "bin" in stdout
+    del s
 
+def test_jail_security():
+    s = Sandbox()
+
+    shutil.copy("/bin/cat", s.root_directory + "bin/")
+    p = s.process([s.root_directory + "bin/cat", os.path.abspath(__file__)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+
+    assert p.returncode != 0
     del s
 
