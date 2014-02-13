5500fae447ee7313a3d0f5136603d9b7912f59ce
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-02-13 10:32:22 +0100

    Added basic logging to greedy

--- src/config.py
+++ src/config.py
@@ -1,2 +1,4 @@
 db_name = 'elif'
 email_domain = 'insa-lyon.fr'
+
+logs_dir = 'logs'
--- src/greedy.py
+++ src/greedy.py
@@ -1,10 +1,19 @@
-
 import time
+import logging
+import os
+
 import mongoengine
-import sandbox
+
 import config
+import sandbox
 import job
 
+# Initializing Greedy's logger
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler(os.path.join(config.logs_dir, 'greedy.log'))
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 class Greedy(object):
     """Greedy application instance polling and executing job from the database"""
@@ -17,19 +26,21 @@ class Greedy(object):
         count = 0
 
         for j in job.Job.objects(processed=False):
+            logger.info("Processing job #{}".format(count))
+
             j.process(self.sandbox)
             j.processed = True
             j.save()
 
-            count +=1
+            count += 1
 
         return count
 
     def run(self):
+        logger.info("Greedy started !")
         while True:
             try:
-                time.sleep(1)
-
+                time.sleep(0.2)
             except KeyboardInterrupt:
                 break
 
