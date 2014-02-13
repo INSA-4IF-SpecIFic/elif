38bc96858d0bc5752f5d7e0a1462e3fd8ba56cc1
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-02-13 10:05:00 +0100

    User creation (with pwd encrypting) + unit tests.

--- src/model/user.py
+++ src/model/user.py
@@ -2,17 +2,36 @@
 # -*- coding: utf-8 -*-
 import sys
 sys.path.append('..')
+import os
+import hashlib
 
 import mongoengine
 
 import config
 
+def generate_salt():
+    return os.urandom(16).encode('base_64')
+
+def hash_password(password, salt):
+    return hashlib.sha512(password + salt).hexdigest()
+
 class User(mongoengine.Document):
     email = mongoengine.StringField(primary_key=True)
     username = mongoengine.StringField(required=True)
+
     secret_hash = mongoengine.StringField(required=True)
+    salt = mongoengine.StringField(required=True)
 
     editor = mongoengine.BooleanField(default=False)
+
+    @staticmethod
+    def new_user(email, username, password, editor=False):
+        user = User(email=email, username=username, editor=editor)
+
+        user.salt = generate_salt()
+        user.secret_hash = hash_password(password, user.salt)
+
+        return user
 
     def clean(self):
         # Emails and usernames are case insensitive
@@ -22,10 +41,15 @@ class User(mongoengine.Document):
         if config.email_domain and not self.email.endswith('@{}'.format(config.email_domain)):
             raise mongoengine.ValidationError(dict(email="Email doesn't pass the email restriction"))
 
+    def valid_password(self, password):
+        return hash_password(password, self.salt) == self.secret_hash
+
 if __name__ == '__main__':
     mongoengine.connect('db_test')
+    User.drop_collection()
 
-    user = User(email="ahmed.kachkach@insa-lyon.fr", username='halflings', secret_hash='IFODJI2973')
+    user = User(email="ahmed.kachkach@insa-lyon.fr", username='halflings', secret_hash='IFODJI2973', salt='2')
     user.save()
+
 
     print User.objects.to_json()
