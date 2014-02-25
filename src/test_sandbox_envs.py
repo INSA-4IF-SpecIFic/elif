eb314d1558d82c0b6a81e084fe04262cb814e930
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-25 11:25:34 +0100

    Tests sandbox.python_env()

---
+++ src/test_sandbox_envs.py
@@ -0,0 +1,26 @@
+
+import subprocess
+import sandbox
+
+
+def test_python_env():
+    s = sandbox.Sandbox()
+    s.add_running_env(sandbox.python_env)
+
+    source_file = s.to_sandbox_basis(s.mktemp(prefix='code_', suffix='.py'))
+
+    with s.open(source_file, 'w') as f:
+        f.write('\n'.join([
+            'print "hello"'
+        ]))
+
+    feedback = s.process(['python', source_file], stdout=subprocess.PIPE)
+    stdout = feedback.stdout.read()
+
+    assert feedback.ended_correctly == True
+    assert feedback.return_code == 0
+    assert stdout == 'hello\n'
+
+
+if __name__ == "__main__":
+    test_python_env()
