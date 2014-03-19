2ee2bd8a1bf9eca1164f348ba172664c2c5ed5f6
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-03-19 17:04:06 +0100

    Added "Palindrome" exercise

--- src/utils.py
+++ src/utils.py
@@ -106,7 +106,10 @@ def test_db():
         test2 = Test(input='2\n', output='2').save()
         test3 = Test(input='2\n', output='2').save()
 
-        exercise = Exercise(author=editor, title="Dummy exercise {}".format(i), description="## This is an exercise\n\n* El1\n* El2",
+        exercise = Exercise(author=editor, title="Dummy exercise {}".format(i),
+                            description="## This is an exercise\n\n" +
+                                        "* You get a single number as an input\n" +
+                                        "* Simply return that number as an output\n",
                             boilerplate_code=config.default_boilerplate_code, reference_code=config.default_boilerplate_code, tags=['sort','trees'])
 
         exercise.tests = [test1, test2, test3]
@@ -115,17 +118,126 @@ def test_db():
 
     # "Doable" exercise
     exercise = Exercise(author=editor, title="Return n²",
-                    description="### Return the given number to the 2 !\n\n* You get a\n* Print a²\n![Alt text](/static/img/cat.jpeg)",
+                    description="### Return the given number to the 2 !\n\n" +
+                                "* You get a\n" +
+                                "* Print a²\n" +
+                                "![Alt text](/static/img/cat.jpeg)",
                     boilerplate_code=config.default_boilerplate_code,
                     reference_code=config.default_boilerplate_code,
                     tags=['algorithms'])
-
     test1 = Test(input='1\n', output='1').save()
     test2 = Test(input='2\n', output='4').save()
     test3 = Test(input='-2\n', output='4').save()
-
     exercise.tests = [test1, test2, test3]
+    exercise.published = True
+    exercise.save()
 
+    # Palindrome exercise
+    reference_code = """
+#include <iostream>
+#include <stack>
+#include <sstream>
+using namespace std;
+
+struct node {
+    node* next;
+    int data;
+    explicit node(int data):node(nullptr,data){}
+    node(node* head,int data):next(head),data(data){}
+};
+
+node* insert(node* head,int data){
+    return new node(head,data);
+}
+
+bool is_palindrome(node* head){
+    auto temp = head;
+    std::stack<node*> s;
+    while(nullptr != temp){
+        s.push(temp);
+        temp = temp->next;
+    }
+    while(!s.empty()){
+        if(head->data != s.top()->data){
+            return false;
+        }
+        s.pop();
+        head = head->next;
+    }
+    return true;
+}
+
+int main() {
+    string line;
+    while(getline(cin,line)){
+        istringstream iss(line);
+        int value;
+        if(iss >> value){
+            auto l = insert(nullptr,value);
+            while(iss >> value){
+                l = insert(l,value);
+            }
+            cout << is_palindrome(l) << endl;
+        }
+    }
+    return 0;
+}
+    """
+
+    boilerplate_code = """
+#include <iostream>
+#include <stack>
+#include <sstream>
+using namespace std;
+
+struct node {
+    node* next;
+    int data;
+    explicit node(int data):node(nullptr,data){}
+    node(node* head,int data):next(head),data(data){}
+};
+
+node* insert(node* head,int data){
+    return new node(head,data);
+}
+
+bool is_palindrome(node* head){
+    // Your code here
+}
+
+int main() {
+    string line;
+    while(getline(cin,line)){
+        istringstream iss(line);
+        int value;
+        if(iss >> value){
+            auto l = insert(nullptr,value);
+            while(iss >> value){
+                l = insert(l,value);
+            }
+            cout << is_palindrome(l) << endl;
+        }
+    }
+    return 0;
+}
+    """
+
+    exercise = Exercise(author=editor, title="Palindrome",
+                description="### Trouver les plaindromes !\n\n" +
+                            "* Chaque ligne de l'entrée standard comprend des entiers séparés par des espaces.\n" +
+                            "* Il faut transformer chaque ligne en une liste chaînée et déterminer si c'est un palindrome.\n" +
+                            "* A chaque ligne de l'entrée standard doit correspondre une ligne de la sortie standard avec un 1 dans le cas d'un palindrome et un 0 sinon.\n" +
+
+                            "![Alt text](http://onapo.files.wordpress.com/2009/04/palyndrome1.gif)",
+                boilerplate_code=boilerplate_code,
+                reference_code=reference_code,
+                tags=['algorithms', 'strings', 'data-structures'],
+                score=42)
+
+    test1 = Test(input='lol', output='1\n').save()
+    test2 = Test(input='car\nracecar', output='0\n1\n').save()
+    test3 = Test(input='car\nracecar'*100, output='0\n1\n'*100).save()
+    exercise.tests = [test1, test2, test3]
     exercise.published = True
     exercise.save()
 
